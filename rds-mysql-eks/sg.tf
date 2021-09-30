56c08a811abe8eddf26853c199db8186810a98a8
ophir.h <ophir.h@quali.com>
2021-09-30 14:36:15 +0300

    add rds-mysql module for eks

---
+++ rds-mysql-eks/sg.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "rds" {
+  name        = "rds-${var.sandbox_id}_sg"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
